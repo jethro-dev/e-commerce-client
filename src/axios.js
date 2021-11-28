e58bb64cae597862059c03fc53f9286fcfcd0a2f
Jethro Au <supergalong@gmail.com>
2021-11-28 01:50:06 +0000

    update axios

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "https://logo-eshop.herokuapp.com/api/v1";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
-  ?.currentUser?.accessToken;
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOTE5N2M2NWI2OWJiNGQ5ZmJjMDlhZCIsImFkbWluIjp0cnVlLCJpYXQiOjE2MzgwNjM4MzYsImV4cCI6MTYzODMyMzAzNn0.PPcQEtfnuuzEcGb5G4ilqPX5cqQD42aKHmbO-lcRJ-Q";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
