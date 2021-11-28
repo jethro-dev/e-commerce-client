ef377367cfac2391fd9482597f7acc1eb35ca5d6
Jethro Au <supergalong@gmail.com>
2021-11-28 00:16:27 +0000

    update axios

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "https://logo-eshop.herokuapp.com/api/v1";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  .currentUser.accessToken;
+const TOKEN =
+  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+    .accessToken || "null";
 
 const instance = axios.create({
   baseURL: BASE_URL,
